c25a9e4f45bd78f067d87c8c9f19f389ea3104da
Attila Domokos <adomokos@gmail.com>
2018-11-05 17:37:49 -0600

    Add genTuple example

--- test/RomanNumeralsSpec.hs
+++ test/RomanNumeralsSpec.hs
@@ -1,6 +1,7 @@
 module RomanNumeralsSpec where
 
 import Test.Hspec
+import Test.QuickCheck
 import Data.List (isPrefixOf, find)
 import Data.Maybe (fromJust)
 
@@ -35,6 +36,13 @@ convertFromRoman r =
   number + convertFromRoman (drop (length roman) r)
     where
       (number, roman) = fromJust $ find (\(_,r') -> r' `isPrefixOf` r) conversions
+
+genTuple :: (Arbitrary a, Arbitrary b)
+         => Gen (a, b)
+genTuple = do
+  a <- arbitrary
+  b <- arbitrary
+  return (a, b)
 
 spec :: Spec
 spec = do
